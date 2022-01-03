71453a8adecccc2e00879fa12baed7223f335787
garronej <joseph.garrone.gj@gmail.com>
2022-01-04 00:53:52 +0100

    Uglier but drier

--- src/app/use-cases/counter.ts
+++ src/app/use-cases/counter.ts
@@ -43,11 +43,19 @@ export const selectors = {
 };
 
 export const thunks = {
-  incrementAsync: createAsyncThunk<number, number, AsyncThunkConfig>('counter/fetchCount', async (amount: number, { extra }) => {
-    const { data } = await extra.counterApi.fetchCount(amount);
+  ...(() => {
 
-    return data;
-  }),
+    const thunkName = "incrementAsync";
+
+    return {
+      [thunkName]: createAsyncThunk<number, number, AsyncThunkConfig>(`${name}/${thunkName}`, async (amount: number, { extra }) => {
+        const { data } = await extra.counterApi.fetchCount(amount);
+
+        return data;
+      })
+    };
+
+  })(),
   incrementIfOdd(amount: number): AppThunk {
     return (dispatch, getState) => {
       const currentValue = selectors.count(getState());
