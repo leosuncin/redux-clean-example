c384e27f76ba70e594335071a425ae74d005f988
garronej <joseph.garrone.gj@gmail.com>
2022-01-26 23:28:18 +0100

    Merge todo app and counter app into one

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { TodoMvc } from './ui/todomvc';
+import { CounterPage } from "./ui/counter";
 import { createStore } from './app/store';
 
 const store = createStore({
@@ -13,6 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <TodoMvc />
+      <CounterPage />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
