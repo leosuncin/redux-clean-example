dce6ea391e4069de90fe93e6355e90e53a8afd20
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-02-02 15:17:00 -0600

    build(counter): setup vitest to run the tests

--- examples/counter/vite.config.ts
+++ examples/counter/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 });
